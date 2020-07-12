868dc7d75202aabdceff369cbe2815712dd575ef
Админко <vladstar25@yandex.ru>
2020-07-12 23:02:54 +0700

    z 1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -15,28 +15,28 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        //UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        //userDaoJDBC.createUsersTable();
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
 
-        //userDaoJDBC.saveUser("Vasya", "Pupkin", (byte) 35);
-        //userDaoJDBC.saveUser("Pasha", "Koshkin", (byte) 25);
-        //userDaoJDBC.saveUser("Merlin", "Monro", (byte) 85);
-        //userDaoJDBC.saveUser("Roman", "Hochkin", (byte) 43);
-/*
+        userDaoJDBC.saveUser("Vasya", "Pupkin", (byte) 35);
+        userDaoJDBC.saveUser("Pasha", "Koshkin", (byte) 25);
+        userDaoJDBC.saveUser("Merlin", "Monro", (byte) 85);
+        userDaoJDBC.saveUser("Roman", "Hochkin", (byte) 43);
+
         Iterator<User> iterator = userDaoJDBC.getAllUsers().iterator();
         while (iterator.hasNext()){
             System.out.println(iterator.next().toString());
         }
 
         userDaoJDBC.dropUsersTable();
-*/
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-        userDaoHibernate.createUsersTable();
-        userDaoHibernate.saveUser("Roman", "Hochkin", (byte) 43);
-        userDaoHibernate.saveUser("Pasha", "Koshkin", (byte) 25);
-        userDaoHibernate.saveUser("Merlin", "Monro", (byte) 85);
-        userDaoHibernate.saveUser("Roman", "Hochkin", (byte) 43);
-        userDaoHibernate.dropUsersTable();
+
+        //UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+        //userDaoHibernate.createUsersTable();
+        //userDaoHibernate.saveUser("Roman", "Hochkin", (byte) 43);
+        //userDaoHibernate.saveUser("Pasha", "Koshkin", (byte) 25);
+        //userDaoHibernate.saveUser("Merlin", "Monro", (byte) 85);
+        //userDaoHibernate.saveUser("Roman", "Hochkin", (byte) 43);
+        //userDaoHibernate.dropUsersTable();
     }
 
 }
